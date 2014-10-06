34b6f8f9c486e78fe26f8db44335ebc4c5d140c3
Eric Herman <eric@freesa.org>
2014-10-06 05:55:05 +0200

    remove un-needed function declaration in public header

--- c/silly_sorting/qsort_ints.h
+++ c/silly_sorting/qsort_ints.h
@@ -5,7 +5,5 @@
 #include <stdlib.h>
 
 void qsort_ints(int *elements, size_t num_elements);
-int compare_ints(int a, int b, int descending);
-int int_comparator(const void *vs_a, const void *vs_b, void *context);
 
 #endif /* _QSORT_INTS_H_ */
