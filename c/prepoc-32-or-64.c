740eb7692f6e1fdf1656aafbfc4e9091ec2afa64
Eric Herman <eric@freesa.org>
2015-01-11 09:05:40 +0100

    a way to test for 32 or 64 bit in the preprocessor

---
+++ c/prepoc-32-or-64.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#ifndef __WORDSIZE
+#define DIFINED_WORDSIZE "not_defined"
+#else
+#if ( __WORDSIZE == 64 )
+#define DIFINED_WORDSIZE "found_64"
+#else
+#if ( __WORDSIZE == 32 )
+#define DIFINED_WORDSIZE "found_32"
+#else
+#define DIFINED_WORDSIZE "not_known"
+#endif
+#endif
+#endif
+
+int main(int argc, char **argv)
+{
+	printf("DIFINED_WORDSIZE: %s\n", DIFINED_WORDSIZE);
+	return sizeof(void *);
+}
