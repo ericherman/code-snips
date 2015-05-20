e5a1f64bcac4d6b8a70485730569c542c192b09a
Eric Herman <eric@freesa.org>
2015-05-20 11:45:59 +0200

    perl implmentation of the shitlord solution

---
+++ perl/print-100-to-1-shitlord.pl
@@ -0,0 +1,34 @@
+# http://www.thousandtyone.com/blog/EasierThanFizzBuzzWhyCantProgrammersPrint100To1.aspx
+#
+# Print 100 to 1.
+# That's it.
+# That was the question.
+# The Catch?
+# You need to start with "for(int i=0;" and continue from there -
+#  you cannot write anything before "for(int i=0;" and
+#  you can't use two loops.
+
+for(my $i=0;
+
+# The "shit_lord solution" was proposed by AEvar Bjarmason,
+#  (https://github.com/avar) which truly made my day.
+# I thought I would adapt the idea to perl, just for fun.
+
+    shit_lord( \$i ) ; $i-- ) {
+
+    print $i, "\n";
+}
+
+{
+    my $init;
+
+    sub shit_lord {
+        my ($i_ref) = @_;
+
+        if ( !$init ) {
+            $$i_ref = 100;
+            $init   = 1;
+        }
+        return $$i_ref;
+    }
+}
