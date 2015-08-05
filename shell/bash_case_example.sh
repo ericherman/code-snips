fa8668a6b2a35aaedd5449101369199f42634a53
Eric Herman <eric@freesa.org>
2015-08-05 10:15:29 +0200

    add comments to break and fall-through syntax

--- shell/bash_case_example.sh
+++ shell/bash_case_example.sh
@@ -4,16 +4,19 @@ case "$1" in
 	'one')
 		echo "one will break"
 		echo "1"
-		;;
+		;; # break
+
 	'two' | '2')
 		echo "two will fall through"
 		echo "2"
-		;&
+		;& # fall-through
+
 	[tT][hH][rR][eE][eE] | '3')
 		echo "three will break"
 		echo "3"
-	;;
+		;; # break
+
 	*) # default
 		echo "Usage: $0 [one|two|three]"
-	;;
+		;; # break (optional)
 esac
