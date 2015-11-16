02790368a7c6b4134326d80fd45cb6eb1d0c5d35
Eric Herman <eric@freesa.org>
2015-11-16 14:06:10 +0100

    update comment

--- c/ternary-weird.c
+++ c/ternary-weird.c
@@ -6,7 +6,32 @@
 
  warning: ISO C forbids omitting the middle term of a ?: expression [-Wpedantic]
 
- ... but with GCC it works ... and seems to resolve to "the item tested or"
+
+ from https://gcc.gnu.org/onlinedocs/gcc-5.2.0/gcc/Conditionals.html :
+
+ The middle operand in a conditional expression may be omitted. Then if the
+ first operand is nonzero, its value is the value of the conditional
+ expression.
+
+ Therefore, the expression
+
+     x ? : y
+
+ has the value of x if that is nonzero; otherwise, the value of y.
+
+ This example is perfectly equivalent to
+
+     x ? x : y
+
+ In this simple case, the ability to omit the middle operand is not
+ especially useful. When it becomes useful is when the first operand
+ does, or may (if it is a macro argument), contain a side effect. Then
+ repeating the operand in the middle would perform the side effect twice.
+ Omitting the middle operand uses the value already computed without the
+ undesirable effects of recomputing it.
+
+
+ See also: https://en.wikipedia.org/wiki/Elvis_operator
 */
 
 #include <stdio.h>
