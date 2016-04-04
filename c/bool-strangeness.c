a67159dc41c11602bb40a25c8d274916d14c4216
Eric Herman <eric@freesa.org>
2016-04-04 23:19:04 +0200

    add reference to C99 Rationale

--- c/bool-strangeness.c
+++ c/bool-strangeness.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+
+char *url = "http://www.open-std.org/jtc1/sc22/wg14/www/C99RationaleV5.10.pdf";
+
 int main(int argc, char **argv)
 {
-	printf("althouth _Bool is technically an integer type,\n");
-	printf(" conversion to _Bool does not always work the\n");
-	printf(" same as conversion to other integer types:\n");
-	printf("  ((int) 0.5) ? 1 : 0 == %d\n", ((int)0.5) ? 1 : 0);
-	printf("((_Bool) 0.5) ? 1 : 0 == %d\n", ((_Bool) 0.5) ? 1 : 0);
+	printf("\nQuote from C99RationaleV5.10.pdf[1] Section: 6.3.1.2:\n");
+	printf("\n\talthouth _Bool is technically an integer type,\n");
+	printf("\tconversion to _Bool does not always work the\n");
+	printf("\tsame as conversion to other integer types\n");
+	printf("\nThis is easy to show:\n");
+	printf("\t  ((int) 0.1) ? T : F == %c\n", ((int)0.1) ? 'T' : 'F');
+	printf("\t((_Bool) 0.1) ? T : F == %c\n", ((_Bool) 0.1) ? 'T' : 'F');
+	printf("\t        (0.1) ? T : F == %c\n", (0.1) ? 'T' : 'F');
+	printf("\n[1] %s\n\n", url);
 
-	return ((int)0.5) || ((_Bool) 0.5);
+	return ((int)0.1) || ((_Bool) 0.1);
 }
