8bffd0b71bc16307e942b73c7b270e1720426b45
Eric Herman <eric@freesa.org>
2016-11-06 13:20:42 +0100

    include addr2line example

--- c/backtrace.c
+++ c/backtrace.c
@@ -20,7 +20,18 @@ $ ./backtrace
  backtrace() and backtrace_symbols_fd() are GNU extensions.
 
  the "-rdynamic" option is needed to see the function names in the output,
- (otherwise one could pass the address to addr2line)
+ otherwise, if "-g" is used, one could pass the address to addr2line:
+
+$ gcc -g -D_POSIX_SOURCE backtrace.c -o backtrace
+$ ./backtrace
+./backtrace[0x804854c]
+./backtrace[0x80485a7]
+./backtrace[0x80485d1]
+/lib/i386-linux-gnu/libc.so.6(__libc_start_main+0xf7)[0xb752a637]
+./backtrace[0x8048441]
+$ addr2line -e backtrace -a 0x80485a7 -f -p
+0x080485a7: my_func at /home/eric/src/code-snips/c/backtrace.c:51
+$
 */
 
 #include <stdio.h>		/* stdout fileno */
