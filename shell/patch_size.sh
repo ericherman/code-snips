b77cd8028e4bf99177dbcd05587b0a47c59b36ff
Eric Herman <eric@freesa.org>
2018-01-24 13:29:59 +0100

    whitespace

--- shell/patch_size.sh
+++ shell/patch_size.sh
@@ -3,11 +3,11 @@
 ADDED=0;
 REMOVED=0;
 
-while read line; 
+while read line;
 do
-  echo "$line" | grep -q "^+" && ADDED=$(($ADDED+1)) 
+  echo "$line" | grep -q "^+" && ADDED=$(($ADDED+1))
   echo "$line" | grep "^-" | grep -vq "^---" && REMOVED=$(($REMOVED+1))
-done < $1 
+done < $1
 
 echo "Added: $ADDED"
 echo "Removed: $REMOVED"
