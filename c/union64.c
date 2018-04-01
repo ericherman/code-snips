4a9ce0d12786df31ff333d29ef5a7e3eb0a9365b
Eric Herman <eric@freesa.org>
2018-04-01 11:53:17 +0200

    How to represent bit-array in union?
    Exploring how to specify an array of bits in C that would be useful
    inside of a Union. I have the feeling that the bitfield syntax
    should support this, but I have yet to sort that out. Nor have I
    found a handy exmaple. For the moment, I have resorted to using
    getter/setter functions, but that feels wrong.

---
+++ c/union64.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+union union64 {
+	uint64_t bits;
+
+	uint8_t bytes[8];
+	uint16_t ushorts[4];
+	uint32_t uints[2];
+	uint64_t ulong;
+
+	int8_t tinys[8];
+	int16_t shorts[4];
+	int32_t ints[2];
+	int64_t slong;
+
+	/* Float32 floats[2]; */
+	/* Float64 doubles[1]; */
+};
+
+uint8_t union64_bit_at(union union64 u64, uint8_t bit)
+{
+	if (bit > 63) {
+		return 0;
+	}
+	return (u64.bits & (1ULL << bit)) ? 1 : 0;
+}
+
+uint8_t union64_bit_set(union union64 u64, uint8_t bit, uint8_t val)
+{
+	uint8_t previous;
+
+	if (bit > 63) {
+		return 0;
+	}
+	previous = (u64.bits & (1ULL << bit)) ? 1 : 0;
+	if (val && !previous) {
+		u64.bits += (1ULL << bit);
+	} else if (!val && previous) {
+		u64.bits -= (1ULL << bit);
+	}
+	return previous;
+}
+
+int main(int argc, char *argv[])
+{
+	uint64_t val;
+	size_t i;
+	union union64 u64;
+
+	if (argc > 1) {
+		val = (uint64_t)strtoll(argv[1], NULL, 10);
+	} else {
+		val = (1ULL << 2) + (1ULL << 60);
+	}
+
+	u64.ulong = val;
+
+	printf("sizeof u64 is %u bytes\n", sizeof(u64));
+
+	printf("u64.ulong = %llu\n", (unsigned long long)u64.ulong);
+
+	for (i = 0; i < 2; ++i) {
+		printf("u64.uints[%u] = %lu\n", (unsigned)i,
+		       (unsigned long)u64.uints[i]);
+	}
+
+	for (i = 0; i < 4; ++i) {
+		printf("u64.ushorts[%u] = %u\n", (unsigned)i,
+		       (unsigned)u64.ushorts[i]);
+	}
+	for (i = 0; i < 8; ++i) {
+		printf("u64.bytes[%u] = %u\n", (unsigned)i,
+		       (unsigned)u64.bytes[i]);
+	}
+
+	for (i = 0; i < 64; ++i) {
+		printf("union64_bit_at(u64, %u) = %u\n", (unsigned)i,
+		       (unsigned)union64_bit_at(u64, i));
+	}
+
+	return 0;
+}
