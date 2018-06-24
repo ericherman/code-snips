00fcb6761ded51737d9868d5f1eb146c1ce26b01
Eric Herman <eric@freesa.org>
2018-06-24 17:52:51 +0200

    add a NOP version

--- c/opencl-vector-func.c
+++ c/opencl-vector-func.c
@@ -4,6 +4,9 @@
 	opencl-vector-func.c \
 	-lOpenCL
 
+   optionally add "-DDEFAULT_TO_NOP=1" to have NOP function
+   which still interacts with the GPU
+
    modified from:
    https://github.com/smistad/OpenCL-Getting-Started/blob/master/main.c
  */
@@ -35,6 +38,14 @@
 			(long)(actual), (long)(expected), msg); \
 		exit(EXIT_FAILURE); } \
 	} while (0)
+
+char *nop_opencl_vector_float_func_src =	/*        these comments  */
+    "__kernel void vector_float_func(\n"	/*        are just to     */
+    "\t\t__global float *a,\n"	/*                        keep the code   */
+    "\t\t__global float *b,\n"	/*                        auto formatter  */
+    "\t\t__global float *c)\n"	/*                        from putting    */
+    "{\n"			/*                        all the strings */
+    "}\n";			/*                        on fewer lines  */
 
 char *opencl_vector_float_func_src =	/*                these comments  */
     "__kernel void vector_float_func(\n"	/*        are just to     */
@@ -134,8 +145,13 @@ int main(int argc, char **argv)
 	if (src_str && buf_size > 1) {
 		printf("using %s\n", argv[1]);
 	} else {
+#if DEFAULT_TO_NOP
+		printf("using nop_opencl_vector_float_func_src\n");
+		src_str = nop_opencl_vector_float_func_src;
+#else
 		printf("using opencl_vector_float_func_src\n");
 		src_str = opencl_vector_float_func_src;
+#endif
 		if (buf_size) {
 			free(src_str);
 			buf_size = 0;
@@ -150,11 +166,15 @@ int main(int argc, char **argv)
 	num_entries = 1;
 	ret = clGetPlatformIDs(num_entries, &platform_id, &ret_num_platforms);
 	assert_long_equal_msg(ret, CL_SUCCESS, "clGetPlatformIDs");
+	printf("platform_id: %ld\n", (long)platform_id);
+	fflush(stdout);
 
 	ret =
 	    clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_ALL, num_entries,
 			   &device_id, &ret_num_devices);
 	assert_long_equal_msg(ret, CL_SUCCESS, "clGetDeviceIDs");
+	printf("device_id: %ld\n", (long)device_id);
+	fflush(stdout);
 
 	/* Create an OpenCL context */
 	num_devices = 1;
@@ -162,29 +182,39 @@ int main(int argc, char **argv)
 	    clCreateContext(NULL, num_devices, &device_id, NULL, NULL, &ret);
 	assert_long_equal_msg(ret, CL_SUCCESS, "clCreateContext");
 	assert_msg((context != NULL), "the context obj");
+	printf("context: %p\n", (void *)context);
+	fflush(stdout);
 
 	/* Create a command queue (this is deprecated 1.2 stuff) */
 	q_props = 0;		/* bit-field, see man 3 clCreateCommandQueue */
 	command_queue = clCreateCommandQueue(context, device_id, q_props, &ret);
 	assert_long_equal_msg(ret, CL_SUCCESS, "clCreateCommandQueue");
 	assert_msg((command_queue != NULL), "the command_queue obj");
+	printf("command_queue: %p\n", (void *)command_queue);
+	fflush(stdout);
 
 	/* Create memory buffers on the device for each local vector */
 	a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, array_bytes_len,
 				   NULL, &ret);
 	assert_long_equal_msg(ret, CL_SUCCESS, "a_mem_obj = clCreateBuffer");
 	assert_msg((a_mem_obj != NULL), "a_mem_obj");
+	printf("a_mem_obj: %p\n", (void *)a_mem_obj);
+	fflush(stdout);
 
 	b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, array_bytes_len,
 				   NULL, &ret);
 	assert_long_equal_msg(ret, CL_SUCCESS, "b_mem_obj = clCreateBuffer");
 	assert_msg((b_mem_obj != NULL), "b_mem_obj");
+	printf("b_mem_obj: %p\n", (void *)b_mem_obj);
+	fflush(stdout);
 
 	res_mem_obj =
 	    clCreateBuffer(context, CL_MEM_WRITE_ONLY, array_bytes_len, NULL,
 			   &ret);
 	assert_long_equal_msg(ret, CL_SUCCESS, "res_mem_obj = clCreateBuffer");
 	assert_msg((res_mem_obj != NULL), "res_mem_obj");
+	printf("res_mem_obj: %p\n", (void *)res_mem_obj);
+	fflush(stdout);
 
 	/* Copy the lists input_a and input_b to their buffers */
 	offset = 0;
@@ -192,17 +222,23 @@ int main(int argc, char **argv)
 	    clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, offset,
 				 array_bytes_len, input_a, 0, NULL, NULL);
 	assert_long_equal_msg(ret, CL_SUCCESS, "clEnqueueWriteBuffer(q, a_mem");
+	printf("clEnqueueWriteBuffer\n");
+	fflush(stdout);
 
 	ret =
 	    clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, offset,
 				 array_bytes_len, input_b, 0, NULL, NULL);
 	assert_long_equal_msg(ret, CL_SUCCESS, "clEnqueueWriteBuffer(q, b_mem");
+	printf("clEnqueueWriteBuffer\n");
+	fflush(stdout);
 
 	/* Create a program from the kernel source */
 	program = clCreateProgramWithSource(context, 1, (const char **)&src_str,
 					    (const size_t *)&src_len, &ret);
 	assert_long_equal_msg(ret, CL_SUCCESS, "clCreateProgramWithSource");
 	assert_msg((program != NULL), "program obj");
+	printf("program:%p\n", (void *)program);
+	fflush(stdout);
 	if (buf_size) {
 		free(src_str);
 		buf_size = 0;
@@ -219,21 +255,31 @@ int main(int argc, char **argv)
 	/* Build the program */
 	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
 	assert_long_equal_msg(ret, CL_SUCCESS, "clBuildProgram");
+	printf("clBuildProgram\n");
+	fflush(stdout);
 
 	/* Create the OpenCL kernel */
 	kernel = clCreateKernel(program, "vector_float_func", &ret);
 	assert_long_equal_msg(ret, CL_SUCCESS, "kernel = clCreateKernel");
 	assert_msg((kernel != NULL), "kernel obj");
+	printf("kernel:%p\n", (void *)kernel);
+	fflush(stdout);
 
 	/* Set the arguments of the kernel */
 	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
 	assert_long_equal_msg(ret, CL_SUCCESS, "clSetKernelArg( &a_mem_obj");
+	printf("clSetKernelArg a\n");
+	fflush(stdout);
 
 	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
 	assert_long_equal_msg(ret, CL_SUCCESS, "clSetKernelArg( &b_mem_obj");
+	printf("clSetKernelArg b\n");
+	fflush(stdout);
 
 	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&res_mem_obj);
 	assert_long_equal_msg(ret, CL_SUCCESS, "clSetKernelArg( &res_mem_obj");
+	printf("clSetKernelArg c\n");
+	fflush(stdout);
 
 	/* Execute the OpenCL kernel on the list */
 	global_item_size = List_len;	/* Process the entire lists */
@@ -243,12 +289,16 @@ int main(int argc, char **argv)
 				   &global_item_size, &process_in_groups_of, 0,
 				   NULL, NULL);
 	assert_long_equal_msg(ret, CL_SUCCESS, "clEnqueueNDRangeKernel");
+	printf("clEnqueueNDRangeKernel\n");
+	fflush(stdout);
 
 	/* Read the memory buffer result on the device to the local result */
 	ret =
 	    clEnqueueReadBuffer(command_queue, res_mem_obj, CL_TRUE, 0,
 				array_bytes_len, result, 0, NULL, NULL);
 	assert_long_equal_msg(ret, CL_SUCCESS, "clEnqueueReadBuffer");
+	printf("clEnqueueReadBuffer\n");
+	fflush(stdout);
 
 	/* Display the local result to the screen */
 	for (i = 0; i < List_len; i++) {
