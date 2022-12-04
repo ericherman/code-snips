cf9efa209650807f023f4e877f2411f866a00d39
Eric Herman <eric@freesa.org>
2022-12-04 11:12:11 +0100

    clean up fix-ssh

--- shell/fix-ssh.sh
+++ shell/fix-ssh.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 # sourcing this file creates the "fix-ssh" command
+#
+# running fix-ssh will:
+# 1) symlink ~/.ssh_auth_sock to the socket with the most recent ctime
+# 2) set SSH_AUTH_SOCK=~/.ssh_auth_sock
+#
+# atime (Access timestamp): last time a file was accessed
+# mtime (Modified timestamp): last time a file's contents were modified
+# ctime (Change timestamp): last time a file's metadata was changed
+# stat(1) uses %X for atime, %Y for mtime, %Z for ctime
+#
+# if any argument is added to fix-ssh, it will be in verbose mode
 
 function fix-ssh() {
+
 	if [ "_${1}_" == "__" ]; then
 		fix_ssh_verbose=0
 	else
@@ -15,26 +27,41 @@ function fix-ssh() {
 		LN=ln
 	fi
 
+	possible_sockets=$(ls -t $SSH_AUTH_SOCK /tmp/ssh*/*agent* 2>/dev/null)
+
 	best_ctime=0
-	for file in $(ls -t $SSH_AUTH_SOCK /tmp/ssh*/*agent*); do
+	best_sock=""
+
+	for file in $possible_sockets; do
 		sfile=$(readlink -f $file)
+
 		if [ $fix_ssh_verbose -gt 0 ]; then
 			ls -l $sfile
 		fi
+
 		if [ -S "$sfile" ]; then
 			ctime=$(stat --format=%Z "$sfile")
+
 			if [ $fix_ssh_verbose -gt 0 ]; then
-				echo "ctime: $ctime"
+				echo "  ctime: $ctime ($(date -d @$ctime))"
 			fi
+
 			if [ $ctime -gt $best_ctime ]; then
-				$LN -sf $sfile ~/.ssh_auth_sock &&
-				export SSH_AUTH_SOCK=~/.ssh_auth_sock &&
+				best_sock=$sfile
 				best_ctime=$ctime
 			fi
 		fi
 	done
+
+	if [ "_${best_sock}_" == "__" ]; then
+		echo "no sockets found"
+		exit 1
+	fi
+
+	$LN -sf $best_sock ~/.ssh_auth_sock &&
+	export SSH_AUTH_SOCK=~/.ssh_auth_sock
+
 	if [ $fix_ssh_verbose -gt 0 ]; then
 		echo SSH_AUTH_SOCK=$SSH_AUTH_SOCK
-		echo "'~/.ssh_auth_sock' -> '$(readlink -f ~/.ssh_auth_sock)'"
 	fi
 }
