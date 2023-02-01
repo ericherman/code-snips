da7b2cf64173843ff203af83dc7b87a22aa40eca
Eric Herman <eric@freesa.org>
2023-02-01 15:54:31 +0100

    expand support

--- shell/spdx.sh
+++ shell/spdx.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 # SPDX-License-Identifier: LGPL-2.1-or-later
-# Copyright (C) 2020 Eric Herman <eric@freesa.org>
+# Copyright (C) 2020-2023 Eric Herman <eric@freesa.org>
+
+if [ "_${VERBOSE}_" != "__" ] && [ "$VERBOSE" -gt 0 ]; then
+	set -x
+fi
+
+IGNORE_PATTERN_DEFAULT='\.json\|LICENSE\|COPYING'
+
+function usage() {
+PROG=`basename $0`
+cat << EOF
+example usage:
+
+	$PROG LGPL-2.1-or-later 'Jane Doe <jd@example.org>'
+
+environment variables:
+
+set USE_SPDX_FILE_COPYRIGHT=1 to enable "SPDX-FileCopyrightText:" prefix
+set SKIP_COPYRIGHT_PREFIX=1 to skip 'Copyright (C)' in the attribution text
+set IGNORE_PATTERN to tell grep to match different files than the default
+	IGNORE_PATTERN_DEFAULT: '$IGNORE_PATTERN_DEFAULT'
+set MARKDOWN_FRONTMATTER=1 to us YAML style in .md that begins with '---'
+EOF
+}
+
+if [ "_${1}_" == "__" ] || [ "_${2}_" == "__" ]; then
+	usage
+	exit 1
+fi
 
 THIS_YEAR=$(date +%Y)
 
@@ -9,9 +37,14 @@ function add_spdx() {
 	SPDX_ID=$2
 	shift 2
 	HOLDER="$@"
+	PRESERVE_FIRST=0
 
 	EXTENSION=${FILE##*.}
 	if [ $EXTENSION == "py" ]; then
+		COMMENT='# '
+		ENDC=''
+	elif [ $EXTENSION == "sh" ]; then
+		PRESERVE_FIRST=1
 		COMMENT='# '
 		ENDC=''
 	elif [ $EXTENSION == "js" ]; then
@@ -28,9 +61,26 @@ function add_spdx() {
 		ENDC=' */'
 	elif [ $EXTENSION == 'sql' ]; then
 		COMMENT='-- '
+	elif [ $EXTENSION == 'tex' ]; then
+		COMMENT='% '
+		ENDC=''
+	elif [ $EXTENSION == 'yml' ]; then
+		COMMENT='# '
+		ENDC=''
+	elif [ $EXTENSION == 'yaml' ]; then
+		COMMENT='# '
+		ENDC=''
 	elif [ $EXTENSION == 'md' ]; then
-		COMMENT='<!-- '
-		ENDC=' -->'
+		if [ "_${MARKDOWN_FRONTMATTER}_" != "__" ] &&
+			[ "${MARKDOWN_FRONTMATTER}" -gt 0 ] &&
+			[ "_$(head -n1 $FILE)_" == "_---_" ]; then
+			PRESERVE_FIRST=1
+			COMMENT='# '
+			ENDC=''
+		else
+			COMMENT='<!-- '
+			ENDC=' -->'
+		fi
 	elif [ $EXTENSION == 'json' ]; then
 		return
 	else
@@ -41,19 +91,64 @@ function add_spdx() {
 	grep SPDX-License-Identifier $FILE
 	if [ $? -eq 0 ]; then return; fi
 
-	echo "${COMMENT}SPDX-License-Identifier: ${SPDX_ID}${ENDC}" > $FILE.0
 	FROM_YEAR=$( git log --diff-filter=A --follow \
 			--format='%ad' --date=format:%Y -1 -- $FILE )
 	if [ "$FROM_YEAR" == "$THIS_YEAR" ]; then
 		YEARS="$THIS_YEAR"
 	else
-		YEARS="$FROM_YEAR - $THIS_YEAR"
+		YEARS="${FROM_YEAR}-${THIS_YEAR}"
 	fi
-	echo "${COMMENT}Copyright (C) $YEARS ${HOLDER}${ENDC}" >> $FILE.0
-	cat $FILE >> $FILE.0
+
+	if [ "_${USE_SPDX_FILE_COPYRIGHT}_" != "__" ] \
+		&& [ "${USE_SPDX_FILE_COPYRIGHT}" -gt 0 ]; then
+		SPDX_COPYRIGHT_PREFIX="SPDX-FileCopyrightText:"
+	else
+		SPDX_COPYRIGHT_PREFIX=""
+	fi;
+	if [ "_${SKIP_COPYRIGHT_PREFIX}_" == "__" ] \
+		|| [ "${SKIP_COPYRIGHT_PREFIX}" -eq 0 ]; then
+		COPYRIGHT_PREFIX="${SPDX_COPYRIGHT_PREFIX} Copyright (C)"
+	else
+		COPYRIGHT_PREFIX="${SPDX_COPYRIGHT_PREFIX}"
+	fi
+
+	touch $FILE.0
+	FILE_LINES=$(cat $FILE | wc -l)
+	if [ $FILE_LINES > 0 ]; then
+		if [ $PRESERVE_FIRST -gt 0 ]; then
+			head -n1 $FILE > $FILE.0
+			FILE_LINES=$(( $FILE_LINES - 1 ))
+		fi
+	fi
+
+	echo "${COMMENT}SPDX-License-Identifier: ${SPDX_ID}${ENDC}" >> $FILE.0
+	echo "${COMMENT}${COPYRIGHT_PREFIX} ${YEARS} ${HOLDER}${ENDC}" >> $FILE.0
+
+	tail -n$FILE_LINES $FILE >> $FILE.0
 	cat $FILE.0 > $FILE
 	rm $FILE.0
 	git diff $FILE
 }
 
-add_spdx $@
+function git_list_files() {
+		# -r Recurse into sub-trees.
+	git ls-tree \
+		--full-tree \
+		-r \
+		--name-only \
+		HEAD
+}
+
+if [ "_${IGNORE_PATTERN}_" == "__" ]; then
+	IGNORE_PATTERN=$IGNORE_PATTERN_DEFAULT
+fi
+
+for FILE in $(git_list_files | grep --invert-match $IGNORE_PATTERN); do
+	if ! [ -f $FILE ]; then
+		continue
+	fi
+	if grep --quiet SPDX-License-Identifier $FILE; then
+		continue
+	fi
+	add_spdx $FILE $@
+done
