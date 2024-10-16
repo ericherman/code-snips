7760a3ac6bc02d324ff251caa0915e0abd8c8aa9
Eric Herman <eric@freesa.org>
2024-10-16 12:48:37 +0200

    clarify inside-out object demo

--- perl/dump-safe-password-demo.pl
+++ perl/dump-safe-password-demo.pl
@@ -1,15 +1,14 @@
 #!/usr/bin/env perl
-package SecretText;
-use strict;
-use warnings;
-use Scalar::Util qw/refaddr/;
+use 5.034;
 
 # It's frustrating when a log accidentally contains a password
-# because of a debug dump of an argument list or similr.
+# because of a debug dump of an argument list or similar.
 #
 # We can avoid that by using "inside out objects" to hide the
 # contents from the log, like this:
 
+package SecretText;
+use Scalar::Util qw/refaddr/;
 {
 	my $secrets = {};    # only accessble inside this block
 
@@ -30,10 +29,15 @@ use Scalar::Util qw/refaddr/;
 }
 1;
 
+package main;
 use Data::Dumper;
 
-my $pw = new SecretText("our little secret");
+print "we create a SecretText object:\n",
+	"\tmy \$pw = new SecretText('our little secret');\n";
+my $pw = new SecretText('our little secret');
 
-print "we can't see it in a string: '$pw'\n";
-print "we can't see it in a Dump: \n\t", Dumper( { pw => $pw } );
-print "yet we can reveal it: '", $pw->reveal, "'\n";
+print "we can't see it in a string, \$pw: '$pw'\n";
+print "we can't see it in a Dumpper( { pw => \$pw } ): \n\t",
+	Dumper( { pw => $pw } );
+print "yet we can retrieve it using \$pw->reveal(): '", $pw->reveal(), "'\n";
+
