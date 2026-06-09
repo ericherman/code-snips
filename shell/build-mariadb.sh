fd97fc45a3a588074b75b88d6a4b304a7ef93f8b
Eric Herman <eric@freesa.org>
2026-06-09 09:31:17 +0200

    add ability to build with nice & ionice

--- shell/build-mariadb.sh
+++ shell/build-mariadb.sh
@@ -2,12 +2,34 @@
 set -ex
 
 PROCS=$( grep -c ^processor /proc/cpuinfo )
-if [ $PROCS -lt 4 ]; then
-	JOBS=3
-else
-	JOBS=$(( $PROCS - 1 ))
-fi
+JOBS=$(( 2 + $PROCS ))
 echo "JOBS=$JOBS"
+
+if [ -z "$NICE" ]; then
+	if ! command -v nice >/dev/null 2>&1; then
+		echo >&2 "no nice? is coreutiles NOT installed?"
+	else
+		NICE="nice -n 10"
+	fi
+fi
+
+if [ -z "$IONICE" ]; then
+	if ! command -v ionice >/dev/null 2>&1 ; then
+		echo >&2 "no ionice? maybe install util-linux?"
+	else
+		# man ionice
+		# 0 for none, 1 for realtime, 2 for best-effort, 3 for idle.
+		IONICE="ionice -c 3"
+	fi
+fi
+
+# -DCMAKE_CFLAGS=-Wno-dev
+# -DENABLE_GCOV=ON
+
+# -DWITHOUT_CSV=True
+# -DWITHOUT_INNODB=True
+# -DWITHOUT_PERFSCHEMA=True
+# -DWITHOUT_TOKUDB=True
 
 time {
 	rm -rf build
@@ -15,11 +37,14 @@ time {
 	cd build
 	cmake .. \
 		-DCMAKE_BUILD_TYPE=Debug \
-		-DWITHOUT_ROCKSDB=True \
-		-DWITHOUT_GROONGA=True \
 		-DWITHOUT_CONNECT=True \
+		-DWITHOUT_GROONGA=True \
+		-DWITHOUT_MROONGA=True \
 		-DWITHOUT_OQGRAPH=True \
-		-DWITHOUT_MROONGA=True
-	make -j $JOBS
+		-DWITHOUT_ROCKSDB=True \
+		"$@"
+
+	$NICE $IONICE make -j $JOBS
+
 	echo YAY
 }
